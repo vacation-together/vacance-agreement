1142ced59b7b34c01bc12003359a3a6ea67f7979
eunhye3333 <eunhye_3@naver.com>
2023-09-02 14:06:03 +0900

    init

---
+++ src/main/java/com/vacance/agreement/VacanceAgreementApplication.java
@@ -0,0 +1,13 @@
+package com.vacance.agreement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacanceAgreementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacanceAgreementApplication.class, args);
+	}
+
+}
