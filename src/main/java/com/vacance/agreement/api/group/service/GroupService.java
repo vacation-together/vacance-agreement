05b352d954f27e81ec3db8d12ddb9ef3d39694ce
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-09-02 16:20:33 +0900

    feat : Group 조회 기능 추가

--- src/main/java/com/vacance/agreement/api/group/service/GroupService.java
+++ src/main/java/com/vacance/agreement/api/group/service/GroupService.java
@@ -4,6 +4,8 @@ import com.vacance.agreement.api.group.data.GroupRepository;
 import com.vacance.agreement.api.group.dto.request.CreateGroupRequest;
 import com.vacance.agreement.api.group.dto.response.GroupIdResponse;
 import com.vacance.agreement.api.group.model.Group;
+import com.vacance.agreement.common.advice.CustomException;
+import com.vacance.agreement.common.response.ResponseCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +19,10 @@ public class GroupService {
         Group group = groupRepository.save(createGroupRequest.toEntity());
 
         return GroupIdResponse.toDto(group);
+    }
+
+    private Group getGroup(long groupId) {
+        return groupRepository.findById(groupId)
+                .orElseThrow(() -> new CustomException(ResponseCode.GROUP_NOT_FOUND));
     }
 }
