6c7c4dce8238fb05b738417ebae7ca9d72e5749d
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-09-02 16:38:07 +0900

    feat : Group 도메인 내 비밀번호 체크 메서드 생성

--- src/main/java/com/vacance/agreement/api/group/model/Group.java
+++ src/main/java/com/vacance/agreement/api/group/model/Group.java
@@ -1,8 +1,11 @@
 package com.vacance.agreement.api.group.model;
 
 import com.vacance.agreement.api.member.model.Member;
+import com.vacance.agreement.common.advice.CustomException;
+import com.vacance.agreement.common.response.ResponseCode;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
@@ -12,6 +15,7 @@ import java.util.List;
 @Getter
 @DynamicInsert
 @DynamicUpdate
+@NoArgsConstructor
 @Entity
 @Table(name = "GROUPS")
 public class Group {
@@ -33,5 +37,11 @@ public class Group {
     private Group(String pw, String title) {
         this.pw = pw;
         this.title = title;
+    }
+
+    public void checkPw(String pw) {
+        if (!this.pw.equals(pw)) {
+            throw new CustomException(ResponseCode.GROUP_PW_NOT_EQ);
+        }
     }
 }
