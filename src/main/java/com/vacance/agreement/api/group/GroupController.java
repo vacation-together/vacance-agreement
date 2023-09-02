379e77b7a70d56cecb8b0edd1891a54b35de7c56
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-09-02 15:50:45 +0900

    feat : Group 생성 로직 추가

--- src/main/java/com/vacance/agreement/api/group/GroupController.java
+++ src/main/java/com/vacance/agreement/api/group/GroupController.java
@@ -1,12 +1,27 @@
 package com.vacance.agreement.api.group;
 
+import com.vacance.agreement.api.group.dto.request.CreateGroupRequest;
+import com.vacance.agreement.api.group.dto.response.GroupIdResponse;
 import com.vacance.agreement.api.group.service.GroupService;
+import com.vacance.agreement.common.response.ApiResponse;
+import com.vacance.agreement.common.response.ResponseCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class GroupController {
 
     private final GroupService groupService;
+
+    @PostMapping("/group")
+    public ResponseEntity<?> createGroup(@Validated @RequestBody CreateGroupRequest createGroupRequest) {
+        GroupIdResponse group = groupService.createGroup(createGroupRequest);
+
+        return new ResponseEntity<>(ApiResponse.ok(ResponseCode.CREATED, group), HttpStatus.CREATED);
+    }
 }
