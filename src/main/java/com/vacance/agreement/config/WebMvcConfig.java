f12d098e9a02ea8c1251b8559ca33385f0d8bbc1
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-09-02 19:46:24 +0900

    feat : Cors 세팅 추가

---
+++ src/main/java/com/vacance/agreement/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.vacance.agreement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
